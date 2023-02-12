c4bc75b82d71a548fa140151fd3d79a6593a9d72
Ryan Bui <buihuyryan@gmail.com>
2023-02-12 12:15:20 -0800

    Add dot files

---
+++ nvim/lua/rbui/core/options.lua
@@ -0,0 +1,34 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start"
+
+-- clipboard
+opt.clipboard:append("unnamedplus")
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+opt.iskeyword:append("-") -- consider string-string as whole word
