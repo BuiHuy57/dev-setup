5059b60b66654a8b9b73261f415659a1bf295a13
Ryan Bui <buihuyryan@gmail.com>
2024-03-29 11:49:33 -0400

    Move files to macOS dir and Linux

---
+++ Linux/nvim/lua/rbui/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+	-- {
+	-- 	"catppuccin/nvim",
+	-- 	priority = 1000,
+	-- 	name = "catppuccin",
+	-- 	config = function()
+	-- 		vim.cmd([[colorscheme catppuccin-mocha]])
+	-- 	end,
+	-- },
+	{
+		"catppuccin/nvim",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "mocha",
+				transparent_background = true,
+			})
+
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
+}
---
+++ macOS/.config/nvim/lua/rbui/plugins/lsp/mason.lua
@@ -0,0 +1,62 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+
+	config = function()
+		-- import mason
+		local mason = require("mason")
+
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+
+		local mason_tool_installer = require("mason-tool-installer")
+
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"arduino_language_server",
+				"clangd",
+				"cmake",
+				"emmet_ls",
+				"graphql",
+				"lua_ls",
+				"marksman",
+				"prismals",
+				"pyright",
+				"rust_analyzer",
+				"tsserver",
+			},
+			-- auto-install configured servers (with lspconfig)
+			automatic_installation = true, -- not the same as ensure_installed
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier", -- prettier formatter
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint", -- python linter
+				"eslint_d", -- js linter
+				"cpplint", -- cpp linter
+				"ast-grep", -- rust linter
+				"markdownlint", -- markdown lint
+				"shfmt", -- shell formatter
+			},
+		})
+	end,
+}
